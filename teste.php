515eb617742e2e274d9a38e075c51b1d54ca63b4
leo220 <leo.cavazzola@hotmail.com>
2021-04-08 20:42:53 -0300

    -

--- teste.php
+++ teste.php
@@ -6,5 +6,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
+//$entityManager->getRepository(Empresa::class);
 
 var_dump($entityManager->getConnection());
