a06ab0c3722ac7d99a7870d5c4b16ef55133868a
leo220 <leo.cavazzola@hotmail.com>
2021-05-01 23:09:10 -0300

    comentando testes

--- src/testes/integracao/usuarioTestes.php
+++ src/testes/integracao/usuarioTestes.php
@@ -1,63 +1,63 @@
 <?php
-
-namespace greenbook\testes\integracao;
-
-use greenbook\helper\EntityManagerFactory;
-use greenbook\model\Usuario;
-use greenbook\repository\UsuarioRepository;
-
-require_once __DIR__ . '/../../../vendor/autoload.php';
-
-$factory = new EntityManagerFactory();
-$repository = $factory->getEntityManager()->getRepository(Usuario::class);
-$repository = repositoryClass($repository);
-
-//____________________Teste de Insert:
-
-$usuario = new Usuario();
-$usuario = $usuario->fromCadastro("nome", "a@a.com", "123");
-
-$result =  $repository->save($usuario);
-
-if($result->getId() > 0){
-    echo "Teste de usuario insert = Sucesso";
-} else
-    echo "Teste de usuario insert = Falho";
-
-echo "<br>";
-
-//_________________________Teste de Find:
-
-$id = $result->getId();
-
-$usuario = $repository->findById($id);
-
-if($usuario ->getId() == $id){
-    echo "Teste de usuario findById = Sucesso";
-} else
-    echo "Teste de usuario findById = Falho";
-
-echo "<br>";
-
-//______________________Teste de Email:
-
-$usuario->setEmail("teste@teste.com");
-$repository->save($usuario);
-
-if($usuario ->getEmail() == "teste@teste.com"){
-    echo "Teste de usuario update = Sucesso";
-} else
-    echo "Teste de usuario update = Falho";
-
-echo "<br>";
-
-$usuario->setEmail("a@a.com");
-$repository->save($usuario);
-
-//_____________________Fim dos testes
-
-function repositoryClass($myClass): UsuarioRepository
-{
-    return $myClass;
-}
-
+//
+//namespace greenbook\testes\integracao;
+//
+//use greenbook\helper\EntityManagerFactory;
+//use greenbook\model\Usuario;
+//use greenbook\repository\UsuarioRepository;
+//
+//require_once __DIR__ . '/../../../vendor/autoload.php';
+//
+//$factory = new EntityManagerFactory();
+//$repository = $factory->getEntityManager()->getRepository(Usuario::class);
+//$repository = repositoryClass($repository);
+//
+////____________________Teste de Insert:
+//
+//$usuario = new Usuario();
+//$usuario = $usuario->fromCadastro("nome", "a@a.com", "123");
+//
+//$result =  $repository->save($usuario);
+//
+//if($result->getId() > 0){
+//    echo "Teste de usuario insert = Sucesso";
+//} else
+//    echo "Teste de usuario insert = Falho";
+//
+//echo "<br>";
+//
+////_________________________Teste de Find:
+//
+//$id = $result->getId();
+//
+//$usuario = $repository->findById($id);
+//
+//if($usuario ->getId() == $id){
+//    echo "Teste de usuario findById = Sucesso";
+//} else
+//    echo "Teste de usuario findById = Falho";
+//
+//echo "<br>";
+//
+////______________________Teste de Email:
+//
+//$usuario->setEmail("teste@teste.com");
+//$repository->save($usuario);
+//
+//if($usuario ->getEmail() == "teste@teste.com"){
+//    echo "Teste de usuario update = Sucesso";
+//} else
+//    echo "Teste de usuario update = Falho";
+//
+//echo "<br>";
+//
+//$usuario->setEmail("a@a.com");
+//$repository->save($usuario);
+//
+////_____________________Fim dos testes
+//
+//function repositoryClass($myClass): UsuarioRepository
+//{
+//    return $myClass;
+//}
+//
--- src/testes/unitarios/publicacao.php
+++ src/testes/unitarios/publicacao.php
@@ -1,59 +1,59 @@
 <?php
-
-namespace greenbook\testes\integracao;
-
-use greenbook\helper\EntityManagerFactory;
-use greenbook\model\Comentario;
-use greenbook\model\Imagem;
-use greenbook\model\Publicacao;
-use greenbook\model\Reacao;
-use greenbook\model\Usuario;
-use greenbook\repository\PublicacaoRepository;
-use greenbook\repository\UsuarioRepository;
-
-require_once __DIR__ . '/../../../vendor/autoload.php';
-
-$factory = new EntityManagerFactory();
-$entityManager = $factory->getEntityManager();
-
-$usuarioRepository = $entityManager->getRepository(Usuario::class);
-$publicacaoRepository = $entityManager->getRepository(Publicacao::class);
-
-$usuarioRepository = usuarioRepositoryClass($usuarioRepository);
-$publicacaoRepository = publicacaoRepositoryClass($publicacaoRepository);
-
-//_________________________Teste add:
-
-$idpub = 5;
-$idUsu = 31;
-
-$publicacao = $publicacaoRepository->findById($idpub);//pega uma publicacao do banco
-$usuario = $usuarioRepository->findById($idUsu);//pega um usuario do banco
-
-$comentario = new Comentario("coment", $usuario, $publicacao);//cria um comentario
-$imagem = new Imagem("http", 100, $publicacao);
-$reacao = new Reacao("like", $usuario);
-
-$publicacao->addComentario($comentario);
-$publicacao->addImagem($imagem);
-$publicacao->addReacao($reacao);
-
-echo $publicacao->getComentarios()->count();
-echo "<br>";
-echo $publicacao->getImagens()->count();
-echo "<br>";
-echo $publicacao->getReacao()->count();
-
-$publicacaoRepository->save($publicacao);
-
-//_____________________Fim dos testes
-
-function publicacaoRepositoryClass($myClass): PublicacaoRepository
-{
-    return $myClass;
-}
-
-function usuarioRepositoryClass($myClass): UsuarioRepository
-{
-    return $myClass;
-}
+//
+//namespace greenbook\testes\integracao;
+//
+//use greenbook\helper\EntityManagerFactory;
+//use greenbook\model\Comentario;
+//use greenbook\model\Imagem;
+//use greenbook\model\Publicacao;
+//use greenbook\model\Reacao;
+//use greenbook\model\Usuario;
+//use greenbook\repository\PublicacaoRepository;
+//use greenbook\repository\UsuarioRepository;
+//
+//require_once __DIR__ . '/../../../vendor/autoload.php';
+//
+//$factory = new EntityManagerFactory();
+//$entityManager = $factory->getEntityManager();
+//
+//$usuarioRepository = $entityManager->getRepository(Usuario::class);
+//$publicacaoRepository = $entityManager->getRepository(Publicacao::class);
+//
+//$usuarioRepository = usuarioRepositoryClass($usuarioRepository);
+//$publicacaoRepository = publicacaoRepositoryClass($publicacaoRepository);
+//
+////_________________________Teste add:
+//
+//$idpub = 5;
+//$idUsu = 31;
+//
+//$publicacao = $publicacaoRepository->findById($idpub);//pega uma publicacao do banco
+//$usuario = $usuarioRepository->findById($idUsu);//pega um usuario do banco
+//
+//$comentario = new Comentario("coment", $usuario, $publicacao);//cria um comentario
+//$imagem = new Imagem("http", 100, $publicacao);
+//$reacao = new Reacao("like", $usuario);
+//
+//$publicacao->addComentario($comentario);
+//$publicacao->addImagem($imagem);
+//$publicacao->addReacao($reacao);
+//
+//echo $publicacao->getComentarios()->count();
+//echo "<br>";
+//echo $publicacao->getImagens()->count();
+//echo "<br>";
+//echo $publicacao->getReacao()->count();
+//
+//$publicacaoRepository->save($publicacao);
+//
+////_____________________Fim dos testes
+//
+//function publicacaoRepositoryClass($myClass): PublicacaoRepository
+//{
+//    return $myClass;
+//}
+//
+//function usuarioRepositoryClass($myClass): UsuarioRepository
+//{
+//    return $myClass;
+//}
